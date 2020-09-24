dd3f206ee3a336547536af618cbd22c4185a714b
David Temple <dstemple7@gmail.com>
2020-09-24 18:26:57 -0500

    mvp minus not clickable cells

--- src/Game.js
+++ src/Game.js
@@ -3,15 +3,17 @@ import './index.css'
 
 const Game = () => {
 
-    const [cellSize, setCellSize] = useState(3)
+    let [cellSize, setCellSize] = useState(5)
     const [canvasWidth, setCanvasWidth] = useState(1800)
-    const [canvasHeight, setCanvasHeight] = useState(750)
+    const [canvasHeight, setCanvasHeight] = useState(700)
     const dead = `#000000`
     const alive = `#FFFFFF`
     const defaultColumns = Math.floor(canvasWidth / cellSize)
     const defaultRows = Math.floor(canvasWidth / cellSize)
-    const [columns, setColumns] = useState(defaultColumns)
-    const [rows, setRows] = useState(defaultRows)
+
+    let [columns, setColumns] = useState(defaultColumns)
+    let [rows, setRows] = useState(defaultRows)
+    let [counter, setCounter] = useState(0)
     let active_array = []
     let inactive_array = []
 
@@ -24,10 +26,6 @@ const Game = () => {
         }
         inactive_array = active_array
     }
-
-    const gameSetUp = () => {
-        arrayInit();
-    };
 
     useEffect(() => {
         arrayInit()
@@ -45,44 +43,49 @@ const Game = () => {
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < columns; j++) {
                 let canvas = document.querySelector("#canvas")
-                let ctx = canvas.getContext("2d")
+                let cellMatrix = canvas.getContext("2d")
                 let color;
                 if (active_array[i][j] === 1)
                     color = alive;
                 else
                     color = dead;
-                ctx.fillStyle = color;
-                ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+                cellMatrix.fillStyle = color;
+                cellMatrix.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
             }
         }
-    };
+    }
 
     const runGame = () => {
-        updateLifeCycle();
-        fillArray();
-    };
-
+        updateLifeCycle()
+        fillArray()
+    }
     
     const start = () => {
+        let count = 0
         randomArray()
         fillArray()
         window.setInterval(() => {runGame()}, 250)
-    }
-
-    const speedUp = () => {
-        randomArray()
-        fillArray()
-        window.setInterval(() => {runGame()}, 25)
-    }
-
-    const slowDown = () => {
-        randomArray()
-        fillArray()
-        window.setInterval(() => {runGame()}, 750)
+        window.setInterval(() => {setCounter(count++)}, 125)
     }
 
     const stop = () => {
-        gameSetUp()
+        window.location.reload(true)
+    }
+
+    const speedUp = () => {
+        let count = 0
+        randomArray()
+        fillArray()
+        window.setInterval(() => {runGame()}, 25)
+        window.setInterval(() => {setCounter(count++)}, 25)
+    }
+
+    const slowDown = () => {
+        let count = 0
+        randomArray()
+        fillArray()
+        window.setInterval(() => {runGame()}, 750)
+        window.setInterval(() => {setCounter(count++)}, 750)
     }
 
     const cellCountNeighbors = (row, col) => {
@@ -108,50 +111,54 @@ const Game = () => {
     };
 
     const updateCellValue = (row, col) => {
-        const total = countNeighbors(row, col);
+        const total = countNeighbors(row, col)
         if (total > 4 || total < 3) {
-            return 0;
+            return 0
         }
         else if (active_array[row][col] === 0 && total === 3) {
-            return 1;
+            return 1
         }
         else {
-            return active_array[row][col];
+            return active_array[row][col]
         }
-    };
+    }
 
     const updateLifeCycle = () => {
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < columns; j++) {
-                let new_state = updateCellValue(i, j);
-                inactive_array[i][j] = new_state;
+                let new_state = updateCellValue(i, j)
+                inactive_array[i][j] = new_state
             }
         }
         active_array = inactive_array
-    };
+    }
 
     const pinkMe = () => {
         var body = document.getElementById("fullBody");
         body.classList.toggle('body_toggle')
       }
 
+    const rules = () => {
+        window.open("https://docs.google.com/document/d/1lzFEJkdYtdykPgNplpbIguoJAXBvQwt6MIsZliJ2Ljg/edit?usp=sharing", '_blank')
+    }
+
     return (
         <div>
             <canvas id="canvas" width={canvasWidth} height={canvasHeight}></canvas>
             <h1>ΖΩΗ</h1>
+            <button>Generation: {counter}</button>
             <div class="buttons-div">
                 <button onClick={start}>start</button>
                 <button onClick={slowDown}>slow down</button>
                 <button onClick={speedUp}>speed up</button>
                 <button onClick={stop}>stop</button>
                 <button id="pinkButton" onClick={pinkMe}>PINK ME!!!</button>
+                <button onClick={rules}>About</button>
             </div>
-            <input 
+            <label style={{color: 'white'}}>Set the Canvas Height </label>
+            <input
                 type="text"
                 style={{backgroundColor:`#FFFFFF`}}
-                placeholder='Canvas Height, init = 800' 
-                min = {200}
-                max = {1600}
                 value={canvasHeight}
                 onChange={e => setCanvasHeight(e.target.value)}
             />
